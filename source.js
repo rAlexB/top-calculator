1e2ec16a1c70e2820b3242e32b8f15583a7b4acb
rAlexB <ruben.barbeiro12@gmail.com>
2026-03-03 12:30:58 +0000

    Added custom font and JS selectors

---
+++ source.js
@@ -0,0 +1,3 @@
+const calculatorDisplay = document.querySelector(".calculator-display");
+const calculatorDigits = document.querySelector(".calculator-digits");
+const calculatorOperators = document.querySelector(".calculator-operators");
