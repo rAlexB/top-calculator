ac449bf3df9e181f622301131625c301485b9d00
rAlexB <ruben.barbeiro12@gmail.com>
2026-03-04 00:14:46 +0000

    Added todo and logic for add digits
    Started implementing the operators

--- source.js
+++ source.js
@@ -2,32 +2,60 @@ const calculatorDisplay = document.querySelector(".calculator-display");
 const calculatorDigits = document.querySelector(".calculator-digits");
 const calculatorOperators = document.querySelector(".calculator-operators");
 
-const validOperators = ["+", "*","-","/"];
-const validDigits = ["1","2","3","4","5","6","7","8","9","0"];
-
 for (const digit in calculatorDigits.childNodes) {
-    digit.addEventListenner("click",)
+    //TODO: acabar a implementação do metodo
+    digit.addEventListenner("click", writeDigit());
 }
 
 
-function writeDigit () {
+//funcao mais simples, simplesmente add nr
+function writeDigit (elem) {
     const displayText = calculatorDisplay.textContent;
     let splitDisplayText = displayText.split(" ");
-    if(validateDisplayText(splitDisplayText)) {
-
+    if(validateDisplayText(splitDisplayText)) {    
+        calculatorDisplay.textContent = calculatorDisplay.textContent + `${elem.value}`;
     }
 
+
+}
+
+function isDigit (digit) {
+
+    const validDigits = ["1","2","3","4","5","6","7","8","9","0"];
+
+    for (const num of validDigits) {
+        if(digit === num) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isOperator (operator) {
+
+    const returnArr = [];
+    
+    const validOperators = ["+", "*","-","/"];
+
+    for (const op of validOperators) {
+        if(op === operator) {
+            returnArr[0] = op;
+            returnArr[1] = true;
+        }
+    }
+
+    return false;
 }
 
 
 function validateDisplayText (splittedArrayText) {
     const firstText = splittedArrayText[0];
-    const foundNum = splittedArrayText.find((elem) => elem == firstText);
     
-    if(firstText === "Welcome") {
+    if(firstText === "Welcome" || firstText === "Input") {
         calculatorDisplay.textContent = "";
         return true;
-    } else if (!(undefined === foundNum)) {
+    } else if (isDigit(firstText)) {
         return true
     } else {
         return false
@@ -35,4 +63,10 @@ function validateDisplayText (splittedArrayText) {
 
     //Welcome... |num + op | num + num
     
+}
+
+
+function inputError () {
+    calculatorDisplay.textContent = "";
+    calculatorDisplay.textContent = "Input is not valid...";
 }
