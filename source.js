9c9ac73860b1fcd048505003cb2ec930cb6266c8
rAlexB <ruben.barbeiro12@gmail.com>
2026-03-12 12:31:58 +0000

    Started implementing math sorting by math precedence

--- source.js
+++ source.js
@@ -87,13 +87,7 @@ function validateDisplayText (splittedArrayText, flagValType = "D") {
 
     } else if(flagValType === "O") {
         if(firstChar == undefined) return false;
-        
-        let count = 1;
-
-        for (const text of splittedArrayText) {
-            if(isOperator(text[0])) count++;
-        }
-        if(count > 1) return false;
+    
         return true;
 
     } else if (isDigit(firstChar)){
@@ -104,22 +98,36 @@ function validateDisplayText (splittedArrayText, flagValType = "D") {
 }
 
 function prepareCalculation() {
-    const displayContent = calculatorDisplay.textContent;
+    let displayContent = calculatorDisplay.textContent;
     clearDisplay();
     const operands = displayContent.trim().split(" ");
 
-    const firstNum = operands[0];
-    const operator = operands [1];
-    const secondNum = operands [2];
-
-    //TODO: lidar com o erro e se calhar concentrar alteracao de valores de display num metodo
-    const result = calculate(operator,+firstNum,+secondNum);
-    if(!(result == undefined)) {
-        calculatorDisplay.textContent = result;
-    } else {
+    if(isOperator(operands.at(-1))) {
         inputError();
+        return;
+    }
+    if(isDigit(operands.at(-1)) && operands.length == 1) {
+        displayContent = operands.at(-1);
     }
 
+    if(operatorsCount(operands) <= 1 && operands.length == 3) {
+        const firstNum = operands[0];
+        const operator = operands [1];
+        const secondNum = operands [2];
+    
+        //TODO: lidar com o erro e se calhar concentrar alteracao de valores de display num metodo
+        const result = calculate(operator,+firstNum,+secondNum);
+        if(!(result == undefined)) {
+            displayContent = result;
+        } else {
+            inputError();
+            return;
+        }
+    }
+
+    
+    calculatorDisplay.textContent = displayContent;
+    return;
 }
 
 function calculate (op, fisrstValue, secondValue) {
@@ -146,3 +154,66 @@ function inputError () {
 function clearDisplay () {
     calculatorDisplay.textContent = "";
 }
+
+function operatorsCount (arr) {
+    let count = 0;
+    for (const op of arr) {
+        if(isOperator(arr)) count++;
+    }
+
+    return count;
+}
+
+function mathPriority (arr) {
+    const priorityOperators = ["*", "/"];
+    const orderedMathArr = [];
+
+    for (const key of arr) {
+        
+        if(priorityOperators.includes(key)) {
+
+            let index = arr.indexOf(key);
+
+            if(arr[index - 1] == undefined) {
+                if(!(isOperator(arr[index]))) {
+                    orderedMathArr.push(arr.splice((index + 1), 1));
+                    orderedMathArr.push(arr.splice((index), 1));
+                    break;
+                } else {
+                    orderedMathArr.push(...arr.splice(index, 2));
+                }
+            }
+            let priorityItems = arr.splice((index - 1), 3);
+            orderedMathArr.unshift(...priorityItems);
+            
+        }
+    }
+
+    for(const elem of arr) {
+        let index = arr.indexOf(elem);
+
+        if(isDigit(elem) && isOperator(arr[index + 1]) && arr.length == 2) {
+            orderedMathArr.push(...arr.splice((index + 1), 1));
+            orderedMathArr.push(...arr.splice((index), 1));
+        } else if (arr.length == 2) {
+            orderedMathArr.push(...arr.splice(index), 2);
+        }
+    }
+
+    orderedMathArr.push(...arr);
+
+    return orderedMathArr;
+
+}
+// + 3
+// 3 + 
+// 3 + 3
+// 1) main: 2 + 1 + 2 / 3
+// 2) second: 2 / 3
+// 2) main: 1 + 
+// 3) second: 2 / 3 + 1 
+// 4) main : undefined
+
+// 1 / 2 * 2
+// 1 + 2 * 3 -2
+//1 + 2 / 3 * 2  -> 2 / 3 1 + * 2 -> 2 / 3 + 1 * 2
