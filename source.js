e62dc63388f511768be7ccd8cf1f41f753d4277a
rAlexB <ruben.barbeiro12@gmail.com>
2026-03-13 18:03:29 +0000

    Application is funcional
    full calculation logic implemented with validaitons

--- source.js
+++ source.js
@@ -4,8 +4,6 @@ const calculatorOperators = document.querySelectorAll(".calculator-operators");
 const calculatorClear = document.querySelector(".calculator-clear");
 const calculatorEquals = document.querySelector(".caluclator-equals");
 
-//TODO: preprar multiplas operaçoes (5+4 -2, analisar primeiro 5+4, depois o -2), nao esquecer da precedencia matemática
-
 document.addEventListener("DOMContentLoaded", (event) => {
     calculatorEquals.addEventListener("click", prepareCalculation);
     calculatorClear.addEventListener("click", clearDisplay);
@@ -78,7 +76,6 @@ function validateDisplayText (splittedArrayText, flagValType = "D") {
     const firstChar = splittedArrayText[0][0];
     const firstText = splittedArrayText[0].join("");
     
-    //TODO: refactor this code (maybe in a switch statement) 
     if(firstText === "Welcome" || firstText === "Input") {
         calculatorDisplay.textContent = "";
         
@@ -108,6 +105,7 @@ function prepareCalculation() {
     }
     if(isDigit(operands.at(-1)) && operands.length == 1) {
         displayContent = operands.at(-1);
+        return;
     }
 
     if(operatorsCount(operands) <= 1 && operands.length == 3) {
@@ -115,19 +113,48 @@ function prepareCalculation() {
         const operator = operands [1];
         const secondNum = operands [2];
     
-        //TODO: lidar com o erro e se calhar concentrar alteracao de valores de display num metodo
         const result = calculate(operator,+firstNum,+secondNum);
         if(!(result == undefined)) {
             displayContent = result;
+            calculatorDisplay.textContent = displayContent;
+            return;
         } else {
             inputError();
             return;
         }
     }
 
-    
-    calculatorDisplay.textContent = displayContent;
-    return;
+    for (let index = 0; index < operands.length; index++) {
+        
+        if(isHighestPrecedenceOperand(operands[index])) {
+            let calculation = calculate(operands[index], +operands[index - 1], +operands[index + 1]);
+            operands.splice(index - 1, 3, calculation);
+            index ++;
+        }
+    }
+
+    let compoundCalculation = 0 ; 
+    while (operands.length != 0) {
+        if(compoundCalculation == 0) {
+            let fisrtCalculationArr = operands.splice(0, 3);
+            compoundCalculation = calculate(fisrtCalculationArr[1], +fisrtCalculationArr[0], +fisrtCalculationArr[2]);
+        }
+
+        if(operands.length == 0) break;
+
+        nextCalculationArr = operands.splice(0,2);
+
+        compoundCalculation = calculate(nextCalculationArr[0], +compoundCalculation, +nextCalculationArr[1]);
+    }
+
+    if(compoundCalculation != 0 && compoundCalculation != undefined) {
+        calculatorDisplay.textContent = compoundCalculation;
+        return;
+    } else {
+        inputError();
+    }
+
+    //2 + 0.5 - 3 + 2 / 2 - 3
 }
 
 function calculate (op, fisrstValue, secondValue) {
@@ -164,62 +191,12 @@ function operatorsCount (arr) {
     return count;
 }
 
-function mathPriority (arr) {
-    const priorityOperators = ["*", "/"];
-    const orderedMathArr = [];
+function isHighestPrecedenceOperand (elem) {
+    const highPrecedenceOperand = ["*", "/"];
 
-    for (const key of arr) {
-        
-        if(priorityOperators.includes(key)) {
-
-            let index = arr.indexOf(key);
-
-            if(arr[index - 1] == undefined) {
-                if(!(isOperator(arr[index]))) {
-                    orderedMathArr.push(arr.splice((index + 1), 1));
-                    orderedMathArr.push(arr.splice((index), 1));
-                    break;
-                } else {
-                    orderedMathArr.push(...arr.splice(index, 2));
-                }
-            }
-            let priorityItems = arr.splice((index - 1), 3);
-            orderedMathArr.unshift(...priorityItems);
-            
-        }
+    for (const operand of highPrecedenceOperand) {
+        if (operand == elem) return true;
     }
 
-    for(const elem of arr) {
-        let index = arr.indexOf(elem);
-
-        /*if(isDigit(elem) && isOperator(arr[index + 1]) && arr.length == 2) {
-            orderedMathArr.push(...arr.splice((index + 1), 1));
-            orderedMathArr.push(...arr.splice((index), 1));*/
-        if(index == 0 && isDigit(elem)) {
-            orderedMathArr.push(...arr.splice((index + 1), 1));
-            orderedMathArr.push(...arr.splice((index), 1));
-        } else if (arr.length == 2) {
-            //orderedMathArr.push(...arr.splice((index), (arr.length - 1)));
-            orderedMathArr.push(...arr);
-            arr = [];
-        }
-        
-    }
-
-    if(!(arr.length == 0)) orderedMathArr.push(...arr);
-
-    return orderedMathArr;
-
+    return false;
 }
-// + 3
-// 3 + 
-// 3 + 3
-// 1) main: 2 + 1 + 2 / 3
-// 2) second: 2 / 3
-// 2) main: 1 + 
-// 3) second: 2 / 3 + 1 
-// 4) main : undefined
-
-// 1 / 2 * 2
-// 1 + 2 * 3 -2
-//1 + 2 / 3 * 2  -> 2 / 3 1 + * 2 -> 2 / 3 + 1 * 2
